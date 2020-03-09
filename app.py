5205fa72506aafd5a6583c78abce6b838c24e3bf
Hylas246 <43715108+Hylas246@users.noreply.github.com>
2020-03-09 11:22:30 +0700

    Update host

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ def index():
     return render_template('main.html', data=data)
 
 if __name__ == '__main__':
-  app.run(host='127.0.0.1', port=5500, debug=True)
+  app.run(host='0.0.0.1', port=5000, debug=True)
